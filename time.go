8492da8c913994887b5acfa7d89b5aacd966fea6
devjoeween <jjazan201@gmail.com>
2022-11-09 14:38:35 +0900

    feat(time): new start, new end

--- time.go
+++ time.go
@@ -13,3 +13,24 @@ func Now() time.Time {
 func NowAfter(d time.Duration) time.Time {
 	return Now().Add(d)
 }
+
+func NewStart(
+	year int,
+	month int,
+	day int,
+) time.Time {
+	t := time.Date(year, time.Month(month), day, 0, 0, 0, 0, dateloc.Current())
+
+	return t
+}
+
+func NewEnd(
+	year int,
+	month int,
+	day int,
+) time.Time {
+	t := time.Date(year, time.Month(month), day, 0, 0, 0, 0, dateloc.Current())
+	t = t.Add((time.Duration(24) * time.Hour) - (time.Duration(1) * time.Nanosecond))
+
+	return t
+}
